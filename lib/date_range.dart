492f942fe29c52b2bfa519e5a9f60c6f3d2e5e36
Aleksei <54249358+Moonspeaker@users.noreply.github.com>
2020-03-11 10:33:19 +0300

    added translation to text

--- lib/date_range.dart
+++ lib/date_range.dart
@@ -4,6 +4,39 @@ class DateRange{
   final DateTime firstDateTime;
   final DateTime lastDateTime;
   const DateRange(this.firstDateTime, this.lastDateTime);
+
+  // ignore: missing_return
+  String get month {
+    final DateTime dateTime = firstDateTime.add(const Duration(days: 13));
+    switch(dateTime.month){
+      case DateTime.january:
+        return "January";
+      case DateTime.february:
+        return "February";
+      case DateTime.march:
+        return "March";
+      case DateTime.april:
+        return "April";
+      case DateTime.may:
+        return "May";
+      case DateTime.june:
+        return "June";
+      case DateTime.july:
+        return "July";
+      case DateTime.august:
+        return "August";
+      case DateTime.september:
+        return "September";
+      case DateTime.october:
+        return "October";
+      case DateTime.november:
+        return "November";
+      case DateTime.december:
+        return "December";
+    }
+  }
+
+  int get year => firstDateTime.add(const Duration(days: 13)).year;
 }
 
 DateTime _lastMonthDateTime(final DateTime firstDateTime){
